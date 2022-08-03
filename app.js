4e0a9f2ddb65ec6e49b281f6ca46df2c71fc79c9
anaharri <anaharringtonb@gmail.com>
2022-08-03 20:01:24 -0300

    add styles with less && dark mode

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
 const progress = document.querySelector("#progress"),
+  circles = document.querySelectorAll(".circle"),
   prev = document.querySelector("#prev"),
   next = document.querySelector("#next"),
-  circles = document.querySelectorAll(".circle");
+  toggleBtn = document.querySelector("#toggle");
 
 let currentActive = 1;
 
 next.addEventListener("click", () => {
   currentActive < circles.length && currentActive++;
-
   update();
 });
 
 prev.addEventListener("click", () => {
   currentActive > 1 && currentActive--;
-
   update();
 });
 
 function update() {
   circles.forEach((circle, idx) => {
-    if (idx < currentActive) {
-      circle.classList.add("active");
-    } else {
-      circle.classList.remove("active");
-    }
+    if (idx < currentActive) circle.classList.add("active");
+    else circle.classList.remove("active");
   });
 
   const actives = document.querySelectorAll(".active");
@@ -38,4 +34,29 @@ function update() {
     prev.disabled = false;
     next.disabled = false;
   }
+}
+
+toggleBtn.addEventListener("click", () => {
+  let allElements = [
+    ...circles,
+    progress,
+    prev,
+    next,
+    toggleBtn,
+    document.querySelector("body"),
+  ];
+
+  allElements.forEach((element) => toggleTheme(element));
+  toggleIcon();
+});
+
+function toggleTheme(element) {
+  element.classList.toggle("dark");
+}
+
+function toggleIcon() {
+  const icon = document.querySelector("i");
+
+  icon.classList.toggle("fa-moon");
+  icon.classList.toggle("fa-sun");
 }
